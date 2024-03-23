c12a31eaeb110a5259a76a43201ae1e69028ad3d
Tobias Eriksson <toberi@outlook.com>
2024-03-23 14:17:42 +0100

    cleanup

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6539dd4e2a486f65d9c2990d9eda5e7e928f523e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06790bc4ab13a9398ba74eb33a33b60ecb95a5d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
