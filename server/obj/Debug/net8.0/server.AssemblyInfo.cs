400bbd0f6d6812f4ddc50a063def2e7ff5b3cd4d
Tobias Eriksson <toberi@outlook.com>
2025-03-02 13:52:44 +0100

    update targetframework dotnet9

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c12a31eaeb110a5259a76a43201ae1e69028ad3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ca660e909880742191c37b4873edf6f1c92b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
